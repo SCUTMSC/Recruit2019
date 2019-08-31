cc3ba1c3b3a08015b4402fc49360371f73efce65
LotteWong <LotteWong21@gmail.com>
2019-08-31 11:30:41 +0800

    云端和本地测试的数据库Url分开

--- express-server/config/database.js
+++ express-server/config/database.js
@@ -1,6 +1,11 @@
 module.exports = {
+    // 部署云端远程的数据库链接
     // remoteUrl : 'mongodb://database/docker-mean',
+    // 部署本机远程的数据库链接
     remoteUrl : 'mongodb://localhost:27017/docker-mean',
-    // localUrl: 'mongodb://database/docker-mean'
+    
+    // 部署云端本地的数据库链接
+    // localUrl: 'mongodb://database/docker-mean',
+    // 部署本机本地的数据库链接
     localUrl : 'mongodb://localhost:27017/docker-mean'
 };
